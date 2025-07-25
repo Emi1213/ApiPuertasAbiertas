ad1cadca61c7904697c26a39574c647528a2bbe7
emilia <emi1213galarza@gmail.com>
2025-07-25 09:45:12 -0500

    Add profile persistence configurations

---
+++ ApiPuertasAbiertas.Infrastructure/Persistence/Configurations/PerfilConfiguration.cs
@@ -0,0 +1,23 @@
+namespace ApiPuertasAbiertas.Infrastructure.Persistence.Configurations;
+
+using ApiPuertasAbiertas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+public class PerfilConfiguration : IEntityTypeConfiguration<Perfil>
+{
+  public void Configure(EntityTypeBuilder<Perfil> builder)
+  {
+    builder.ToTable("Perfiles");
+
+    builder.HasKey(p => p.Id);
+
+    builder.Property(p => p.Nombre)
+        .IsRequired()
+        .HasMaxLength(100);
+
+    builder.Property(p => p.Descripcion)
+        .HasMaxLength(500);
+
+    builder.HasMany(p => p.Usuarios).WithOne().HasForeignKey(e => e.PerfilId);
+  }
+}
