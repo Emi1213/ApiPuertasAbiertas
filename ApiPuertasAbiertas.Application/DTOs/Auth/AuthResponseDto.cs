1a30ba1c9253b909979f4fb32e3ae89bcc8e4716
emilia <emi1213galarza@gmail.com>
2025-07-25 16:50:23 -0500

    Modified dtos

--- ApiPuertasAbiertas.Application/DTOs/Auth/AuthResponseDto.cs
+++ ApiPuertasAbiertas.Application/DTOs/Auth/AuthResponseDto.cs
@@ -3,6 +3,6 @@ namespace ApiPuertasAbiertas.Application.DTOs.Auth;
 public class AuthResponseDto
 {
   public required string Token { get; set; }
-  public DateTime Expiration { get; set; }
+  public DateTime Expiracion { get; set; }
   public required string Usuario { get; set; }
 }
--- ApiPuertasAbiertas.Application/DTOs/Auth/LoginDto.cs
+++ ApiPuertasAbiertas.Application/DTOs/Auth/LoginDto.cs
@@ -3,5 +3,5 @@ namespace ApiPuertasAbiertas.Application.DTOs.Auth;
 public class LoginDto
 {
   public required string Usuario { get; set; }
-  public required string Contrasena { get; set; }
+  public required string Contrasenia { get; set; }
 }
