c3b7a127535da19591711a12c7cea320d6c58b12
emilia <emi1213galarza@gmail.com>
2025-08-14 09:36:55 -0500

    Add repository interface to actions

---
+++ ApiPuertasAbiertas.Domain/Repositories/IModuloRepository.cs
@@ -0,0 +1,13 @@
+using ApiPuertasAbiertas.Domain.Entities;
+
+namespace ApiPuertasAbiertas.Domain.Repositories;
+
+public interface IModuloRepository
+{
+  Task<(int total, List<Modulo>)> BuscarAsync(string? busqueda, int pagina, int tamanioPagina);
+
+  Task<Modulo?> ObtenerPorIdAsync(int id);
+  Task CrearAsync(Modulo modulo);
+  Task ActualizarAsync(Modulo modulo);
+  Task EliminarAsync(int id);
+}
